fe52a5948fdd8b5a69d3d28de19f06313d720aea
mike stedman <github@ravuya.com>
2015-01-17 14:02:02 -0700

    Templatized measurement editor, trying to modernize units by way of making more templates

--- editor.js
+++ editor.js
@@ -1,3 +1,32 @@
+/**
+  Represents a unit of measure.
+  @constructor
+  @param {string} unit - The suffix of the unit (e.g. 'm')
+  @param {string} conversion_multiplier - What to multiply a measurement in meters by
+                                          in order to get this unit.
+*/
+var Unit = function(unit, conversion_multiplier) {
+  var self = this;
+  self.unit = unit;
+  self.conversion_multiplier = conversion_multiplier;
+  self.to_meters = function(unit_value) {
+    return unit_value / conversion_multiplier;
+  };
+  self.from_meters = function(meters) {
+    return unit_value * conversion_multiplier;
+  };
+};
+
+var Measure = function(measurement, unit) {
+  if(typeof(unit) !== 'object') {
+    throw "Expected unit argument to be an object, got " + typeof(unit);
+  }
+
+  var self = this;
+  self.measurement = measurement;
+  self.unit = unit;
+};
+
 var editorViewModel = {
   // A "node" is a point connecting two pipe segments within the pipeline.
   nodes: ko.observableArray(
@@ -5,30 +34,27 @@ var editorViewModel = {
   ),
   // Defines the available units and their conversion strategies.
   units: ko.observableArray([
-    {
-      unit: 'ft',
-      converter_to: function(meters) { return meters * 3.28084; },
-      converter_from: function(feet) { return feet / 3.28084; } // TODO: clean this up; no reason to write this twice.
-    },
-    {
-      unit: 'm',
-      converter_to: function(meters) { return meters; },
-      converter_from: function(meters) { return meters; }
-    },
-    {
-      unit: 'in',
-      converter_to: function(meters) { return meters * 39.3701; },
-      converer_from: function(inches) { return inches / 39.3701; }
-    }
+    new Unit('ft', 3.28084),
+    new Unit('m', 1.0),
+    new Unit('in', 39.3701)
   ]),
   // Pipeline metadata.
   name: "Test Pipeline",
   // All units are internally assumed to be in meters
-  diameter: 0.50,
-  diameter_unit: ko.observable()//ko.observable(units[0])
+  diameter: ko.observable(),
+  // Methods
+  init: function() {
+    // TODO: Has to be a cleaner way to do these defaults.
+    this.diameter = ko.observable(new Measure(0.50, this.units()[1]));
+  },
+  addNodeRow: function() {
+    var meters = this.units[1];
+    var nv = new NodeViewModel(0, meters, 0, meters, 0, meters);
+    this.nodes.push(nv);
+  }
 };
 
-var nodeViewModel = function(x, x_unit, y, y_unit, z, z_unit) {
+var NodeViewModel = function(x, x_unit, y, y_unit, z, z_unit) {
   var self = this;
 
   // TODO: add display observables to handle unit conversion?
@@ -46,6 +72,8 @@ var nodeViewModel = function(x, x_unit, y, y_unit, z, z_unit) {
 };*/
 
 //editorViewModel.addDemoNodes();
+
+editorViewModel.init();
 
 // Initialize knockout.
 ko.applyBindings(editorViewModel);
